b3d87f58ba17cd0d8aca4a686fcbc48e6697da39
Alex Huang <alex.huang@orbitremit.com>
2019-12-09 17:38:29 +1300

    create Profile model

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Mail\NewUserWelcomeMail;
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/email', function () {
-    return new NewUserWelcomeMail();
-});
-Route::post('follow/{user}', 'FollowsController@store');
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/p/create', 'PostsController@create');
-// Route::post('/p', 'PostsController@store');
-// Route::get('/p/{post}', 'PostsController@show');
+Auth::routes();
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
-Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
-Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
 
 
 
